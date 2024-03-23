80fe297b5ce292ad94a8ba6e942c41a08b6d9b7d
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-03-23 16:53:30 -0400

    multisig deposits to zero

--- runtime/devnet/src/lib.rs
+++ runtime/devnet/src/lib.rs
@@ -574,18 +574,18 @@ impl pallet_motion::Config for Runtime {
 }
 
 parameter_types! {
-	// One storage item; key size is 32; value is size 4+4+16+32 bytes = 56 bytes.
-	pub const DepositBase: Balance = deposit(1, 88);
-	// Additional storage item size of 32 bytes.
-	pub const DepositFactor: Balance = deposit(0, 32);
+	// One storage item; key size is 32; value is size 4+4+16+32 bytes = 56 bytes. But we set deposits to zero.
+	pub const MultisigDepositBase: Balance = ZERO_DEPOSIT;
+	// Additional storage item size of 32 bytes, but we set deposits to zero.
+	pub const MultisigDepositFactor: Balance = ZERO_DEPOSIT;
 }
 
 impl pallet_multisig::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
 	type Currency = Balances;
-	type DepositBase = DepositBase;
-	type DepositFactor = DepositFactor;
+	type DepositBase = MultisigDepositBase;
+	type DepositFactor = MultisigDepositFactor;
 	type MaxSignatories = ConstU32<100>;
 	type WeightInfo = pallet_multisig::weights::SubstrateWeight<Runtime>;
 }
